1833d135d6e7dbc8fd25a473628b2b54fd890961
Dustin Fox <dustinfox1996@gmail.com>
2021-02-15 11:57:12 -0500

    Add project files.

---
+++ GeometricShapes project/SQR.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeometricShapes_project {
+    class SQR {
+        public int Side1 { get; set; }
+
+        public int Perimeter() {
+            return 4 * Side1;
+        }
+
+        public int Area() {
+            return Side1 * Side1;
+        }
+        public SQR() {
+        }
+        public SQR(int s1) {
+            Side1 = s1;
+
+        }
+
+
+    }
+}
